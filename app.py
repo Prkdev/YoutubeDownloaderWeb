e83250baef92d58fda143e81419250aa163ef006
Prkdev <64211816+Prkdev@users.noreply.github.com>
2025-06-06 21:11:14 +0900

    Add Streamlit YouTube playlist downloader

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+import tempfile
+import streamlit as st
+from yt_dlp import YoutubeDL
+
+st.title("YouTube Playlist Downloader")
+
+playlist_url = st.text_input("Playlist URL")
+
+cookie_file = st.file_uploader("Upload cookies.txt (optional)", type=["txt"])
+
+DOWNLOAD_DIR = "downloads"
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+if st.button("Download Playlist"):
+    if playlist_url:
+        ydl_opts = {
+            'outtmpl': os.path.join(DOWNLOAD_DIR, '%(playlist_title)s/%(title)s.%(ext)s'),
+            'ignoreerrors': True,
+            'nocheckcertificate': True
+        }
+        tmp_cookie_path = None
+        if cookie_file is not None:
+            with tempfile.NamedTemporaryFile(delete=False, mode='wb', suffix='.txt') as tmp:
+                tmp.write(cookie_file.read())
+                tmp_cookie_path = tmp.name
+            ydl_opts['cookiefile'] = tmp_cookie_path
+
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([playlist_url])
+
+        if tmp_cookie_path:
+            os.remove(tmp_cookie_path)
+
+        st.success("Download completed.")
+    else:
+        st.error("Please provide a playlist URL.")
